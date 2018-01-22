71c7a145b83f0e317d2283194ae06e0dbc832c33
Jade <862226994@qq.com>
2018-01-22 18:26:54 +0800

    xg

--- app/Http/Controllers/Im/LoginController.php
+++ app/Http/Controllers/Im/LoginController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
+    protected $redirectTo = '/home';
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
