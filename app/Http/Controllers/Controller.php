0612719218b5d208f97ff627da503073418f7eff
Jade <862226994@qq.com>
2018-01-24 11:11:35 +0800

    xg

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,12 +11,24 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * 失败重定向
+     * @param string $mas
+     * @param string $redirect_url
+     * @return $this
+     */
     public function errorRedirect($mas='失败！', $redirect_url='')
     {
         if($redirect_url) return redirect()->withErrors(['error_msg' => $mas]);
         return redirect()->back()->withErrors(['error_msg' => $mas])->withInput();
     }
 
+    /**
+     * 成功重定向
+     * @param string $mas
+     * @param string $redirect_url
+     * @return $this
+     */
     public function successRedirect($mas='成功！', $redirect_url='')
     {
         if($redirect_url) return redirect()->withErrors(['success_msg' => $mas]);
