8a4d6616e5535f0030a6d2320b6972456bdef1af
Jade <862226994@qq.com>
2018-01-26 11:41:32 +0800

    xg

--- app/Http/Controllers/Im/IndexController.php
+++ app/Http/Controllers/Im/IndexController.php
@@ -17,21 +17,4 @@ class IndexController extends Controller
 
     }
 
-    /**
-     * 服务器自动拉取代码
-     */
-    public function webhooks()
-    {
-        $secret    = "123456";
-        $path      = "/home/www/im";
-        $signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
-        if ($signature) {
-            $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
-            if (strcmp($signature, $hash) == 0) {
-                echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
-                exit();
-            }
-        }
-        http_response_code(404);
-    }
 }
